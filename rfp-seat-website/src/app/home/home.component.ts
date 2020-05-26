6ddcdc82067f89c53a7b804e725e199ce64b69ae
David G <compresdavid@gmail.com>
2020-05-26 16:41:58 +0200

    fix: add typos + modal test + parallax wip

--- rfp-seat-website/src/app/home/home.component.ts
+++ rfp-seat-website/src/app/home/home.component.ts
@@ -9,13 +9,20 @@ import Parallax from 'parallax-js';
 })
 export class HomeComponent implements OnInit {
 
+  reloadBtn;
+  timerEl;
+
   constructor() { }
 
   ngOnInit(): void {
-    var scene = document.getElementById('scene');
-    var parallaxInstance = new Parallax(scene);
+    // PARALLAX
+    const scene = document.getElementById('scene');
+    const parallaxInstance = new Parallax(scene, { relativeInput: false });
     parallaxInstance.friction(0.2, 0.2);
-  }
 
+    // COUNTER
+    this.reloadBtn = document.querySelector('.reload');
+    this.timerEl = document.querySelector('.timer');
+  }
 
 }
