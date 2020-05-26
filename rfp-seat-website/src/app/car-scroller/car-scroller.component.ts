3523ec12803d042d6d0f6dc58a568e2f9d8c1e56
David G <compresdavid@gmail.com>
2020-05-26 13:16:04 +0200

    fix: delay scroll car

--- rfp-seat-website/src/app/car-scroller/car-scroller.component.ts
+++ rfp-seat-website/src/app/car-scroller/car-scroller.component.ts
@@ -32,8 +32,10 @@ export class CarScrollerComponent implements OnInit {
     const viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
     const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
     const documentHeight = $(document).height();
-    const scrollPositionRelative = scrollPosition / (documentHeight - viewportHeight);
-    return scrollPositionRelative;
+    const homeHeight = $('#home-container').height();
+    const scrollPositionRelative = (scrollPosition - homeHeight) / (documentHeight - viewportHeight - homeHeight);
+    const percentageResult = (scrollPosition - $('#home-container').height() ) < 0 ? 0 : scrollPositionRelative;
+    return percentageResult;
   }
 
   private updateMovable() {
@@ -43,7 +45,7 @@ export class CarScrollerComponent implements OnInit {
   }
 
   private updateAnimation() {
-    if (this.scrollPercentage > 0.1) {
+    if (this.scrollPercentage > 0) {
       $('#navigation-bar').addClass('animate__slideInDown');
       $('#navigation-bar').removeClass('animate__slideOutUp');
     } else {
