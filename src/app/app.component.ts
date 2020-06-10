4f60d1ab064188f2c1ed9080b5d963478d08f897
David G <compresdavid@gmail.com>
2020-06-10 18:41:12 +0200

    feat: mobile disable wrapper

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   @Input() discoveringPathSuccess = true;
 
   @Input() dragAndDropSuccess = false;
+
+  isNotMobile = true;
+
+  ngOnInit(): void {
+    const isMobileDevice = /Mobi/i.test(window.navigator.userAgent);
+    this.isNotMobile = !isMobileDevice;
+  }
 
   discoverPath() {
     this.discoveringPathSuccess = true;
