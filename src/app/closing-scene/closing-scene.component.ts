93b8dfdfc770a4453bcd4a60e65c7b2e2f26ca12
David G <compresdavid@gmail.com>
2020-10-02 15:30:24 +0200

    feature: update assets + styles
    TODO: Optimize images or reduce asset files?

--- src/app/closing-scene/closing-scene.component.ts
+++ src/app/closing-scene/closing-scene.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-closing-scene',
   templateUrl: './closing-scene.component.html',
-  styleUrls: ['./closing-scene.component.css']
+  styleUrls: ['./closing-scene.component.css'],
 })
-export class ClosingSceneComponent implements OnInit {
+export class ClosingSceneComponent {
+  disabledButtons: boolean[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.disabledButtons = [false, true, true];
   }
 
+  nextStep(step: number) {
+    for (let i = 0; i <= step; i++) {
+      this.disabledButtons[i] = false;
+    }
+  }
+
+  resetButtonsState() {
+    this.disabledButtons = [false, false, false];
+  }
 }
